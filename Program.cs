8737f3a52dcd96c1cf8e0bacd4c96ad3fffc864b
Mariam Khattab <116390513+N00ne-00@users.noreply.github.com>
2022-12-19 21:51:17 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+
+            Get_Middle(input);
+
+
+
+        }
+
+        static string Get_Middle(string input)
+        {
+            if (input.Length % 2 == 0)
+            {
+                Console.WriteLine(input.Substring((input.Length / 2) - 1, 2));
+                return input;
+            }
+            else
+            {
+                Console.WriteLine(input[input.Length / 2]);
+                return input;
+            }
+
+
+
+
+
+        }
+
+
+
+
+    }
+    }
+
